b42ae431ed686acca671896739de9aec35949971
Benjamin Urquhart <24701883+BenjaminUrquhart@users.noreply.github.com>
2019-08-01 13:52:34 -0400

    E

--- src/net/benjaminurquhart/forget/Forget.java
+++ src/net/benjaminurquhart/forget/Forget.java
@@ -1,16 +1,37 @@
 package net.benjaminurquhart.forget;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.util.stream.Collectors;
+
 public class Forget {
 
 	public static void main(String[] args) {
 		String program = "";
 		
-		int ptr = 0;
-		for(char c : "\n!dlroW ,olleH".toCharArray()) {
-			program+="const 0x"+Integer.toHexString(ptr++)+" "+(int)c+";";
+		if(args.length > 0) {
+			try {
+				File file = new File(args[0]);
+				if(!file.exists()) {
+					System.out.println("Failed to find file "+args[0]);
+					return;
+				}
+				program = Files.lines(file.toPath()).collect(Collectors.joining());
+			}
+			catch(Exception e) {
+				System.err.println("Error reading file:");
+				e.printStackTrace();
+				return;
+			}
 		}
-		for(int i = 0; i < ptr; i++) {
-			program+="cleanse 0x"+Integer.toHexString(ptr-i-1)+";pop;out;";
+		else {
+			int ptr = 0;
+			for(char c : "\n!dlroW ,olleH".toCharArray()) {
+				program+="const 0x"+Integer.toHexString(ptr++)+" "+(int)c+";";
+			}
+			for(int i = 0; i < ptr; i++) {
+				program+="cleanse 0x"+Integer.toHexString(ptr-i-1)+";pop;out;";
+			}
 		}
 		System.out.println("Program:\n"+program+"\n\n");
 		
